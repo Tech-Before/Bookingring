b3ea6209597d5d2016f0e4bef0bb6e7e58082258
unknown <asifu6698@gmail.com>
2021-11-18 12:21:39 +0500

    Router added for pages

--- Admin/controllers/homeController.js
+++ Admin/controllers/homeController.js
@@ -1,12 +1,126 @@
+// Dashboard
 const indexView = (req, res, next) => {
-    res.render('./pages/home');
+    res.render('./pages/Home/home');
 }
 
-const iconView = (req, res, next) => {
-    res.render('./pages/icons');
+// Areas
+const addArea = (req, res, next) => {
+    res.render('./pages/Areas/addAreas');
+}
+
+const listAreas = (req, res, next) => {
+    res.render('./pages/Areas/areaList');
+}
+
+// Customers
+const customersList = (req, res, next) => {
+    res.render('./pages/Customers/customer')
+}
+
+// Hotels Clients
+const hotelClients = (req, res, next) => {
+    res.render('./pages/Hotels/addHotel')
+}
+
+const hotelList = (req, res, next) => {
+    res.render('./pages/Hotels/hotelsList')
+}
+
+const hotelApproved = (req, res, next) => {
+    res.render('./pages/Hotels/approvedHotels')
+}
+
+const hotelUnapproved = (req, res, next) => {
+    res.render('./pages/Hotels/unapprovedHotels')
+}
+
+const addGalleryHotel = (req, res, next) => {
+    res.render('./pages/Hotels/addHotelGallery')
+}
+
+// Appartments / Houses
+const appartmentsHouses = (req, res, next) => {
+    res.render('./pages/Appartments/addAppartment')
+}
+
+const appartmentsApproved = (req, res, next) => {
+    res.render('./pages/Appartments/appartmentApprovedList')
+}
+
+const approvedappartmentsUnapproved = (req, res, next) => {
+    res.render('./pages/Appartments/appartmentUnapprovedList')
+}
+
+const addGalleryAppartment = (req, res, next) => {
+    res.render('./pages/Appartments/addGalleryAppartments')
+}
+
+const appartmentList = (req, res, next) => {
+    res.render('./pages/Appartments/appartmentList')
+}
+
+const housesList = (req, res, next) => {
+    res.render('./pages/Appartments/housesList')
+}
+
+// Vehicle
+const addVehicle = (req, res, next) => {
+    res.render('./pages/Vehicles/addVehicles')
+}
+
+const vehicleList = (req, res, next) => {
+    res.render('./pages/Vehicles/vehicleList')
+}
+
+// Company Info
+const aboutUs = (req, res, next) => {
+    res.render('./pages/CompanyInfo/aboutUs')
+}
+
+const contactUs = (req, res, next) => {
+    res.render('./pages/CompanyInfo/contactUs')
+}
+
+// Updates / Blog
+const addUpdates = (req, res, next) => {
+    res.render('./pages/Updates/addUpdates')
+}
+
+const updateList = (req, res, next) => {
+    res.render('./pages/Updates/updateList')
+}
+
+// Services
+const addServices = (req, res, next) => {
+    res.render('/pages/Services/addServices')
+}
+
+const servicesList = (req, res, next) => {
+    res.render('./pages/Services/servicesList')
 }
 
 module.exports = {
     indexView,
-    iconView
+    addArea,
+    listAreas,
+    customersList,
+    hotelClients,
+    hotelList,
+    hotelApproved,
+    hotelUnapproved,
+    addGalleryHotel,
+    appartmentsHouses,
+    appartmentsApproved,
+    approvedappartmentsUnapproved,
+    addGalleryAppartment,
+    appartmentList,
+    housesList,
+    addVehicle,
+    vehicleList,
+    aboutUs,
+    contactUs,
+    addUpdates,
+    updateList,
+    addServices,
+    servicesList
 }
--- Admin/routes/home-routes.js
+++ Admin/routes/home-routes.js
@@ -1,9 +1,56 @@
+const { render } = require('ejs');
 const express = require('express');
-const {indexView, iconView} = require('../controllers/homeController')
+const {
+    indexView,
+    addArea,
+    listAreas,
+    customersList,
+    hotelClients,
+    hotelList,
+    hotelApproved,
+    hotelUnapproved,
+    addGalleryHotel,
+    appartmentsHouses,
+    appartmentsApproved,
+    approvedappartmentsUnapproved,
+    addGalleryAppartment,
+    appartmentList,
+    housesList,
+    addVehicle,
+    vehicleList,
+    aboutUs,
+    contactUs,
+    addUpdates,
+    updateList,
+    addServices,
+    servicesList
+} = require('../controllers/homeController')
 const router = express.Router();
 
 router.get('/', indexView)
-router.get('/icons', iconView)
+router.get('/pages/Areas/addAreas', addArea)
+router.get('/pages/Areas/areaList', listAreas)
+router.get('/pages/Customers/customer', customersList)
+router.get('/pages/Hotels/addHotel', hotelClients)
+router.get('/pages/Hotels/hotelsList', hotelList)
+router.get('/pages/Hotels/approvedHotels', hotelApproved)
+router.get('/pages/Hotels/unapprovedHotels', hotelUnapproved)
+router.get('/pages/Hotels/addHotelGallery', addGalleryHotel)
+router.get('/pages/Appartments/addAppartment', appartmentsHouses)
+router.get('/pages/Appartments/appartmentApprovedList', appartmentsApproved)
+router.get('/pages/Appartments/appartmentUnapprovedList', approvedappartmentsUnapproved)
+router.get('/pages/Appartments/addGalleryAppartments', addGalleryAppartment)
+router.get('/pages/Appartments/appartmentList', appartmentList)
+router.get('/pages/Appartments/housesList', housesList)
+router.get('/pages/Vehicles/addVehicles', addVehicle)
+router.get('/pages/Vehicles/vehicleList', vehicleList)
+router.get('/pages/CompanyInfo/aboutUs', aboutUs)
+router.get('/pages/CompanyInfo/contactUs', contactUs)
+router.get('/pages/Updates/addUpdates', addUpdates)
+router.get('/pages/Updates/updateList', updateList)
+router.get('/pages/Services/addServices', addServices)
+router.get('/pages/Services/servicesList', servicesList)
+
 
 module.exports = {
     routes: router
