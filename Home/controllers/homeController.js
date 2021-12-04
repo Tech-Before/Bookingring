5a08e68cabb8d65d0fb266f397c582ad387f4df3
unknown <asifu6698@gmail.com>
2021-12-04 16:43:24 +0500

    Home, Services, News, About, Contact Pages added!

--- Home/controllers/homeController.js
+++ Home/controllers/homeController.js
@@ -1,14 +1,42 @@
 // HomePage
 const home = (req, res, next) => res.render('./pages/HomePage/home');
 
-// Appartments
+// Services (Appartments)
 const appartments = (req, res, next) => res.render('./pages/Appartments/appartments');
 const hotelRooms = (req, res, next) => res.render('./pages/Appartments/hotelRooms');
+const vehicles = (req, res, next) => res.render('./pages/Appartments/vehicles');
+const galleryAppRoom = (req, res, next) => res.render('./pages/Appartments/galleryAppRoom');
+
+// Tours
+const tours = (req, res, next) => res.render('./pages/Tours/tours');
+const hike = (req, res, next) => res.render('./pages/Tours/hike');
+const gallerytandh = (req, res, next) => res.render('./pages/Tours/gallerytandh');
+
+// News
+const news = (req, res, next) => res.render('./pages/News/news');
+
+// About Us
+const about = (req, res, next) => res.render('./pages/About/about');
+
+// Contact 
+const contact = (req, res, next) => res.render('./pages/Contact/contact');
 
 module.exports = {
     // HomePage
     home,
 
-    // Appartments
-    appartments, hotelRooms
+    // Services (Appartments)
+    appartments, hotelRooms, vehicles, galleryAppRoom,
+
+    // Tours
+    tours, hike, gallerytandh,
+
+    // News
+    news,
+
+    // About
+    about,
+
+    // Contact
+    contact
 }
--- Home/routes/home-routes.js
+++ Home/routes/home-routes.js
@@ -4,8 +4,20 @@ const {
     // HomePage
     home,
 
-    // Appartment
-    appartments, hotelRooms
+    // Services (Appartments)
+    appartments, hotelRooms, vehicles, galleryAppRoom,
+
+    // Tours
+    tours, hike, gallerytandh,
+
+    // News
+    news,
+
+    // About Us
+    about,
+
+    // Contact
+    contact
 
 } = require('../controllers/homeController');
 const router = express.Router();
@@ -13,9 +25,26 @@ const router = express.Router();
 // HomePage
 router.get('/', home)
 
-// Appartment
+// Services (Appartments)
 router.get('/Appartments/appartments', appartments)
 router.get('/Appartments/hotelRooms', hotelRooms)
+router.get('/Appartments/vehicles', vehicles)
+router.get('/Appartments/galleryAppRoom', galleryAppRoom)
+
+// Tours
+router.get('/Tours/tours', tours)
+router.get('/Tours/hike', hike)
+router.get('/Tours/gallerytandh', gallerytandh)
+
+// News
+router.get('/News/news', news)
+
+// About Us
+router.get('/About/about', about)
+
+// Contact
+router.get('/Contact/contact', contact)
+
 
 module.exports = {
     routes: router
